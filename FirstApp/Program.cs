cbc114568dfe82cfec8c12e6785ed614b2d38fd6
Liza <elizavetaprokofeva15@gmail.ru>
2024-12-29 16:33:10 +0300

    Добавьте файлы проекта.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+                string myName;
+                myName = "Liza";
+
+                Console.WriteLine(myName);
+
+                Console.ReadKey();
+            
+        }
+    }
+    
+}
